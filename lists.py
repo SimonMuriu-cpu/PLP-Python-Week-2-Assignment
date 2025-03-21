79f23684cc262c5020fe5387b710713d12cc85a8
Simon Muriu <muriusimon6@gmail.com>
2025-03-21 23:26:12 +0300

    First commit of Week 2 Assignment

---
+++ lists.py
@@ -0,0 +1,23 @@
+# 1. Create an empty list
+my_list = []
+
+# 2. Appending elements to the list: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Inserting the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# 4. Extending the list with the values 50, 60, 70
+my_list.extend([50, 60, 70])
+
+# 5. Removing the last element from the list
+my_list.pop()
+
+# 6. Sorting my_list in ascending order
+my_list.sort()
+
+# 7. Find and print the index of the element 30 in the my_list
+print(my_list.index(30))
